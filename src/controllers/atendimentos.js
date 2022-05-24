25f4f2aaaf1f5376c5488654b120c5169c0c2280
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-05-24 11:01:32 -0300

    Criando funções listar e exibir

--- src/controllers/atendimentos.js
+++ src/controllers/atendimentos.js
@@ -1,13 +1,23 @@
- const Atendimentos = require("../models/Atendimentos");
+const Atendimentos = require("../models/Atendimentos");
 
 const atendimentosController = {
     listar : async (req, res) =>{
-        await Atendimentos.sync({}) 
-    } ,
-    exibir : async () =>{
 
+      const listar = await Atendimentos.findAll(); 
+      res.json(listar);
+
+    } ,
+    exibir : async (req, res) =>{
+        const id = req.params.id;
+        const exibir = await Atendimentos.findByPk(id);
+        if(!exibir){
+            return res.status(404).json({mensagem : "não encontrado nenhum dado a partir desse id"});
+        }
+
+        res.json(exibir);
     },
-    agendar : async ( ) =>{
+    agendar : async (req,res) =>{
+
 
     },
 }
