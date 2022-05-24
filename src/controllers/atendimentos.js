60cd61b1c146586ed37d73ce0d946d20c08e054a
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-05-24 00:49:17 -0300

    Iniciando o crud do atendimento

---
+++ src/controllers/atendimentos.js
@@ -0,0 +1,17 @@
+ const Atendimentos = require("../models/Atendimentos");
+
+const atendimentosController = {
+    listar : async (req, res) =>{
+        // await   Atendimentos.sync({
+        //     force :true
+        //}) 
+    } ,
+    exibir : async () =>{
+
+    },
+    agendar : async ( ) =>{
+
+    },
+}
+
+module.exports = atendimentosController;
---
+++ src/models/Atendimentos.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../database");
+
+const Atendimentos = db.define(
+  "atendimentos",
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey:     true,
+        
+      },
+    id_paciente: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    id_psicologo: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    data_atendimento: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    observacao: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   
+  },
+  { tableName: "atendimentos", timestamps: false, underscored: true }
+);
+
+module.exports = Atendimentos;
