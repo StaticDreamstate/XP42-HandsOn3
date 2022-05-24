52278dbba7dcd44de721440ede7fd1a53fa614fb
Jonathan <jonathan.wpc@gmail.com>
2022-05-24 17:15:56 -0300

    Bud do id nulo corrigido

--- src/controllers/atendimentos.js
+++ src/controllers/atendimentos.js
@@ -3,25 +3,23 @@ const db = require('../database');
 
 const atendimentosController = {
     listar : async (req, res) =>{
-
       const listar = await Atendimentos.findAll(); 
       res.json(listar);
-
     } ,
     exibir : async (req, res) =>{
         const id = req.params.id;
+
         const exibir = await Atendimentos.findByPk(id);
         if(!exibir){
-            return res.status(404).json({mensagem : "não encontrado nenhum dado a partir desse id"});
+            return res.status(404).json({mensagem : "Id não encontrado"});
         }
 
         res.json(exibir);
     },
     agendar : async (req,res) =>{
-        
-        const {id,id_paciente, data_atendimento, observacao, id_psicologo} = req.body;
+        const { id_paciente, data_atendimento, observacao, id_psicologo } = req.body;
+
         const novoAgendamento = await Atendimentos.create({
-            id,
             id_psicologo,
             id_paciente,
             data_atendimento,
@@ -29,10 +27,9 @@ const atendimentosController = {
         });
         if(!novoAgendamento){
             return res.status(400).json({mensagem: "os dados não foram preenchidos corretamente, tente de novo!"});
-        } else
-        if(novoAgendamento){
-            return res.status(201).json(novoAgendamento);
         }
+
+        res.status(201).json(novoAgendamento);
     },
 }
 
--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 const Psicologo = require("./Psicologos");
 const Paciente = require('./Paciente')
+const Atendimentos = require('./Atendimentos');
 
 
 
-module.exports = { Psicologo, Paciente };
+module.exports = { Psicologo, Paciente, Atendimentos };
