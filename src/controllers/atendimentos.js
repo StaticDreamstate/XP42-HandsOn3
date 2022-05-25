f36c4af4194386654860e1b32fa646af222cda60
Jonathan <jonathan.wpc@gmail.com>
2022-05-25 12:01:37 -0300

    CRUD atendimentos finalizado

--- src/controllers/atendimentos.js
+++ src/controllers/atendimentos.js
@@ -1,15 +1,32 @@
-const Atendimentos = require("../models/Atendimentos");
-const Paciente = require("../models/Paciente");
+const { Psicologo, Paciente, Atendimentos } = require("../models");
 
 const atendimentosController = {
     listar : async (req, res) =>{
-      const listar = await Atendimentos.findAll(); 
-      res.json(listar);
+        const listar = await Atendimentos.findAll({
+            attributes: ["id", "data_atendimento", "observacao"],
+            include: [
+                Paciente,
+                {
+                    model: Psicologo,
+                    attributes: ["id", "nome", "email", "apresentacao"]
+                }
+            ]
+        }); 
+        res.json(listar);
     } ,
     exibir : async (req, res) =>{
         const id = req.params.id;
 
-        const exibir = await Atendimentos.findByPk(id);
+        const exibir = await Atendimentos.findByPk(id, {
+            attributes: ["id", "data_atendimento", "observacao"],
+            include: [
+                Paciente,
+                {
+                    model: Psicologo,
+                    attributes: ["id", "nome", "email", "apresentacao"]
+                }
+            ]
+        });
         if(!exibir){
             return res.status(404).json({mensagem : "Id não encontrado"});
         }
@@ -22,7 +39,7 @@ const atendimentosController = {
         const paciente = await Paciente.findByPk(paciente_id);
 
         if (!paciente) {
-            return res.json({mensagem: "Id não existente"})
+            return res.json({mensagem: "Id do paciente inválido"});
         }
 
         const novoAgendamento = await Atendimentos.create({
@@ -33,10 +50,21 @@ const atendimentosController = {
         });
         
         if(!novoAgendamento){
-            return res.status(400).json({mensagem: "os dados não foram preenchidos corretamente, tente de novo!"});
+            return res.status(400).json({mensagem: "Dados invalidos"});
         }
 
-        res.status(201).json(novoAgendamento);
+        const resultado = await Atendimentos.findByPk(novoAgendamento.id, {
+            attributes: ["id", "data_atendimento", "observacao"],
+            include: [
+                Paciente,
+                {
+                    model: Psicologo,
+                    attributes: ["id", "nome", "email", "apresentacao"]
+                }
+            ]
+        })
+
+        res.status(201).json(resultado);
     },
 }
 
--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,22 @@ const Psicologo = require("./Psicologos");
 const Paciente = require('./Paciente')
 const Atendimentos = require('./Atendimentos');
 
+Atendimentos.belongsTo(Paciente, {
+    constraint: true,
+    foreignKey: "paciente_id"
+});
+Paciente.hasMany(Atendimentos, {
+    constraint: true,
+    foreignKey: "paciente_id"
+})
 
+Atendimentos.belongsTo(Psicologo, {
+    constraint: true,
+    foreignKey: "psicologo_id"
+});
+Psicologo.hasMany(Atendimentos, {
+    constraint: true,
+    foreignKey: "psicologo_id"
+})
 
 module.exports = { Psicologo, Paciente, Atendimentos };
